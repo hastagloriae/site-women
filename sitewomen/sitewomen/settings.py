f273c5d546aa6eb6d8000904c82a086ba87211a1
kiril <kkornienko885@gmail.com>
2024-07-30 17:42:12 +0300

    The configuration of the URL is complete.

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@!@6s&ww4bu19s!a^3h+f698jgp7m+#n@oxbbxz@_s2j+e&-7*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,10 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return HttpResponse("Сторінка додатку Women")
 
 
-def categories(request):
-    return HttpResponse("<h1>Категорії</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Категорії</h1><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Категорії</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    if year > 2024:
+        return redirect('home')
+
+    return HttpResponse(f"<h1>Архів</h1><p>{year}</p>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Сторінки не існує</h1>")
